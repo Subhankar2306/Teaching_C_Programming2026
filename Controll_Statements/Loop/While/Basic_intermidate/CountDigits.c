c923fe1d94710f70acf312ac0055a888a8b97fd5
Subhankar <subhankardhal637@gmail.com>
2026-08-11 16:56:20 +0530

    Count Digits and sum of that digits

---
+++ Controll_Statements/Loop/While/Basic_intermidate/CountDigits.c
@@ -0,0 +1,21 @@
+// Write a C program to count the number of digits 
+
+#include <stdio.h>
+
+int main()
+{
+    int n, count = 0;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    while(n != 0)
+    {
+        n = n / 10;
+        count++;
+    }
+
+    printf("Number of digits = %d", count);
+
+    return 0;
+}
